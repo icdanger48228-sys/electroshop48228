fef63883e8cac93718d1e433865be6ba51b48ca7
icdanger48228-sys <icdanger48228@gmail.com>
2026-04-06 18:32:18 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@ const urlsToCache = [
 
 //agregado
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/service-worker.js")
+  navigator.serviceWorker.register("/electroshop48228/sw.js")
     .then(registration => {
       console.log("Service Worker registrado con éxito:", registration);
     })

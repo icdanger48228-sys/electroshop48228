054e14472b944a9d762bd62cfc005113f3cf0e08
icdanger48228-sys <icdanger48228@gmail.com>
2026-04-06 10:21:45 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "electroshop-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/afiliados.html",
+  "/planes.html",
+  "/offline.html",
+  "/imagenes/icono.png"
+];
+
+// Instalar y guardar en caché
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Activar y limpiar cachés viejos
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Interceptar peticiones
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request).then(response => {
+        return response || caches.match("/offline.html");
+      });
+    })
+  );
+});
